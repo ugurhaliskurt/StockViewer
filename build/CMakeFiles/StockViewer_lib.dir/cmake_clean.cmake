e65e843f7262c754a7bb1d81c37112aa6ba6275c
Ugur Halis KURT <ugurkurt90@gmail.com>
2022-07-06 23:33:18 +0300

    Delete build directory

--- build/CMakeFiles/StockViewer_lib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libStockViewer_lib.pdb"
-  "libStockViewer_lib.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/StockViewer_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/StockViewer_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libStockViewer_lib.a"
-)
